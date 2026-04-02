3565cefbed6e141c8f9865f33b112436c45bb902
ArdaKorkmaz0 <korkmazarda4455@gmail.com>
2026-04-03 01:21:28 +0300

    Fixed urls

--- tweetapp/urls.py
+++ tweetapp/urls.py
@@ -59,4 +59,5 @@ urlpatterns = [
     path('push/subscribe/', views.push_subscribe, name='push_subscribe'),
     path('push/unsubscribe/', views.push_unsubscribe, name='push_unsubscribe'),
     path('push/vapid-key/', views.vapid_public_key, name='vapid_public_key'),
+    path('', include('django.contrib.auth.urls')),
 ]
